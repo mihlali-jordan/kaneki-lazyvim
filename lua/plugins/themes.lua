f37ff06c8e327b776662c31396049b851417d0ee
mihlali-jordan <biz@jordanmihlali.dev>
2023-07-28 09:10:18 +0200

    feat: update theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,10 +1,12 @@
 return {
   { "Mofiqul/vscode.nvim" },
   { "projekt0n/github-nvim-theme" },
+  { "nyoom-engineering/oxocarbon.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vscode",
+      -- colorscheme = "vscode",
+      colorscheme = "oxocarbon",
     },
   },
 }
